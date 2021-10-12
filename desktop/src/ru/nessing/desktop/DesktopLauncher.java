659207f4177adc6eff8834599d7ac69c2fcec62a
Nessing <35237174+Nessing@users.noreply.github.com>
2021-10-13 01:48:21 +0300

    Create repository

---
+++ desktop/src/ru/nessing/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.nessing.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.nessing.GameGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameGdx(), config);
+	}
+}
